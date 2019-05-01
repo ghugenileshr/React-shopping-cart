05dd11e69617338f86340a2dffeb5e98e204b286
Ghuge Nilesh <43292807+ghugenileshr@users.noreply.github.com>
2019-05-01 15:54:09 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import Navbar from "./Components/Navbar/Navbar";
+import TourList from './Components/TourList';
+import './App.scss';
+
+
+
+
+class App extends Component {
+  render() {
+   return(
+     <main>
+       <Navbar/>
+     <TourList/>
+     </main>
+     
+   )
+  }
+}
+
+export default App;
---
+++ src/Components/tourData.js
@@ -0,0 +1,27 @@
+export const tourData=[
+    { id:1,
+       city:"new york",
+       img:"./img/newyork.jpg",
+       name:"newyork bridge tool",
+       info:"Lorem Ipsum is simply dummy text of the printing and typesetting industry"
+},
+
+  { id:2,
+    city:"paris",
+    img:"./img/paris.jpg",
+    name:"paris bridge tool",
+    info:"Lorem Ipsum is simply dummy text of the printing and typesetting industry"
+},
+{ id:3,
+    city:"london",
+    img:"./img/london.jpg",
+    name:"london bridge tool",
+    info:"Lorem Ipsum is simply dummy text of the printing and typesetting industry"
+},
+{ id:4,
+    city:"tokyo",
+    img:"./img/tokyo.jpg",
+    name:"tokyo bridge tool",
+    info:"Lorem Ipsum is simply dummy text of the printing and typesetting industry"
+}
+];
